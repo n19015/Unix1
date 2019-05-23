9c05f795db6659ab787c1a213a502bf61b3b58c6
fujino tatsunori <n19015@std.it-college.ac.jp>
2019-05-23 12:01:33 +0900

    5/23課題

--- 0523/052303.sh
+++ 0523/052303.sh
@@ -1,1 +1,1 @@
-ls -R /home | grep -v ^\s*$ |sort | head -n 5 | rev | cut -d / -f 1 | rev
+ls -R /home | grep -v ^\s*$ | grep -v ':$' |sort | head -n 5 | rev | cut -d / -f 1 | rev
--- 0523/052304.sh
+++ 0523/052304.sh
@@ -1,1 +1,1 @@
-ls -R /home | grep -v ^\s*$ | sort -r | head -n 5 | rev | cut -d / -f 1 | rev
+ls -R /home | grep -v ^\s*$ | grep -v ':$'|sort -r | head -n 5 | rev | cut -d / -f 1 | rev
