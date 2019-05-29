03120eef2b93cf4cc64db3bf8287a18b1a5f0903
fujino tatsunori <n19015@std.it-college.ac.jp>
2019-05-29 09:46:25 +0900

    5/28課題

--- 0528/052805.sh
+++ 0528/052805.sh
@@ -1,3 +1,3 @@
 #/varディレクトリ以下(サブディレクトリも含む)にあるファイルの容量が0のファイルをすべて表示する
-#find /var -type f 2>/dev/null | xargs du -b 2>/dev/null | sort |grep '^0'
+#find /var -type f 2>/dev/null | xargs sudo du -b 2>/dev/null | sort |grep '^0'
  ls -la 2>/dev/null $(find /var -type f -size 0 2>/dev/null) | sort -nr -k 5
