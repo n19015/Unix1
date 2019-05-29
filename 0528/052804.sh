d7423a452e8b9964067a0c84fc537c42379710a0
fujino tatsunori <n19015@std.it-college.ac.jp>
2019-05-29 09:43:57 +0900

    5/28課題

--- 0528/052804.sh
+++ 0528/052804.sh
@@ -1,3 +1,3 @@
 #/varディレクトリ以下(サブディレクトリも含む)にあるファイルの容量の多い上位5件を表示する
-#find /var -type f 2>/dev/null | xargs du -b 2>/dev/null | sort -nr |head -n 5
+#find /var -type f 2>/dev/null | xargs sudo du -b 2>/dev/null | sort -nr |head -n 5
  ls -la 2>/dev/null $(find /var -type f 2>/dev/null) | sort -nr -k 5 | head -n 5
