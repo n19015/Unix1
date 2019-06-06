75d55db945078aea14c5773eddd4d1d5dd2768dc
fujino tatsunori <n19015@std.it-college.ac.jp>
2019-06-06 09:52:44 +0900

    6/4課題

--- 0604/unix2.sh
+++ 0604/unix2.sh
@@ -9,22 +9,29 @@ echo 'continue...'
 }
 cd $HOME/repo/unix1
 git status
-cont
-echo 'a.git add-i b.git commit c.git push'
+while :
+do
+echo 'a.git add-i b.git commit c.git push *.exit'
 read a
 case "$a" in
     a)
         git add -i
         git status
+
         ;;
    b)
         echo -n 'comment:'
         read comment
         git commit -m "$commentii"
         git log --online
+
         ;;
     c)
         git push
+
+        ;;
+    *)
+        exit
         ;;
 esac
-
+done
