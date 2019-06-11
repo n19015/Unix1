50af67dfecb2e9c5c8b2b10cdb44cc14e0c8c91b
fujino tatsunori <n19015@std.it-college.ac.jp>
2019-06-11 11:15:53 +0900

    6/4課題

--- 0604/060404.sh
+++ 0604/060404.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d "$1" ]; then
-    find $1 -maxdepth 1 -and -type f -not -perm -u=r 
+    find $1 -maxdepth 1 -and -type f -not -perm -a=r
 else
     echo "$1: ディレクトリではありません"
 fi
